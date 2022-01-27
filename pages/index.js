c2f2513af8356cac5c9fb5551e0daa0b3fce08be
Gabriel Melo <gabriel.melo@frentecorretora.com.br>
2022-01-26 23:30:44 -0300

    Aula 2 - UseState e UseRouter

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,22 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import appConfig from "../config.json";
+import React from "react";
+import {useRouter} from "next/router";
 
-import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
-  const Tag = props.tag || 'h1';
+  const Tag = props.tag || "h1";
   return (
     <>
       <Tag>{props.children}</Tag>
       <style jsx>{`
-            ${Tag} {
-                color: ${appConfig.theme.colors.neutrals['000']};
-                font-size: 24px;
-                font-weight: 600;
-            }
-            `}</style>
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals["000"]};
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
     </>
   );
 }
@@ -61,93 +35,143 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'GabrielMelo2303';
+  // const username = 'GabrielMelo2303';
+  const [username, setUsername] = React.useState("GabrielMelo2303");
+  const router = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          backgroundImage:
+            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/allianz-arena-1536x864.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
         }}
       >
         <Box
           styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
             flexDirection: {
-              xs: 'column',
-              sm: 'row',
+              xs: "column",
+              sm: "row",
             },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            width: "100%",
+            maxWidth: "700px",
+            borderRadius: "20px",
+            padding: "32px",
+            margin: "16px",
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 70%)",
+            backgroundColor: appConfig.theme.colors.neutrals[400],
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={ (event) => {
+              event.preventDefault(); // preventDefault() tira o reload do form
+              console.log('Alguem submeteu ao formulário');
+              router.push('/chat')
+              //window.location.href = '/chat' ;
+            }}
             styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: { xs: "100%", sm: "50%" },
+              textAlign: "center",
+              marginBottom: "32px",
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[300],
+              }}
+            >
               {appConfig.name}
             </Text>
 
+            {/* <input 
+              type="text" 
+              value={username}
+              onChange={ (event) => {
+                console.log("o usúario digitou", event.target.value);
+
+                // Onde esta o valor?
+                const valor = event.target.value;
+
+                // Trocar valor da variavel
+               //  Atráves do React 
+                setUsername(valor);
+              }} 
+            /> */}
+
             <TextField
+              value={username}
+              onChange={(event) => {
+                console.log("o usúario digitou", event.target.value);
+
+                // Onde esta o valor?
+                const valor = event.target.value;
+
+                // Trocar valor da variavel
+                //  Atráves do React
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
-                  textColor: appConfig.theme.colors.neutrals[300],
-                  mainColor: appConfig.theme.colors.neutrals[900],
+                  textColor: appConfig.theme.colors.neutrals[100],
+                  mainColor: appConfig.theme.colors.neutrals[100],
                   mainColorHighlight: appConfig.theme.colors.primary[100],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
             />
             <Button
-              type='submit'
-              label='Entrar'
+              type="submit"
+              label="Entrar"
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
                 mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
+                mainColorLight: appConfig.theme.colors.primary[500],
+                mainColorStrong: appConfig.theme.colors.primary[900],
               }}
             />
           </Box>
           {/* Formulário */}
 
-
           {/* Photo Area */}
           <Box
             styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "200px",
+              padding: "16px",
               backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
+              border: "1px solid",
               borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
+              borderRadius: "10px",
               flex: 1,
-              minHeight: '240px',
+              minHeight: "240px",
             }}
           >
             <Image
               styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
+                borderRadius: "50%",
+                marginBottom: "16px",
               }}
               src={`https://github.com/${username}.png`}
             />
@@ -156,8 +180,8 @@ export default function PaginaInicial() {
               styleSheet={{
                 color: appConfig.theme.colors.neutrals[200],
                 backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px'
+                padding: "3px 10px",
+                borderRadius: "1000px",
               }}
             >
               {username}
